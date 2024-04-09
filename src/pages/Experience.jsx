d24cb01efc085c6442c55b49b31d152c6cd3f230
as2759 <as2759@cornell.edu>
2024-04-09 14:30:43 -0400

    adding informatino to the experience page:

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -20,21 +20,24 @@ const Experience = () => (
         <img src={google} className='logo-google'></img>
         <div className='experience-overview-text'> 
           <Link to="/google" > <Tab value="Google" label="Search and Sustainability @ Google" className="experience-overview-link" /> </Link>
-          <p> description </p>
+          <p> Worked on the Sustainability team within Google Search to improve customization of results shown during car searches and to show financial incentives and charging information for cars on desktop devices.  </p>
+          <p>C++/Java/Javascript</p>
         </div>
       </div>
       <div className="experience-overview">
         <img src={ibm} className='logo-ibm'></img>
         <div className='experience-overview-text'> 
           <Link to="/ibm" > <Tab value="IBM" label="Quantum Computing @ IBM" className="experience-overview-link" /> </Link>
-          <p> description </p>
+          <p> Worked on the IBM Quantum Infrastructure team to build a fullstack internal tool that displays high and low severity incidents on microservices that run on IBMs Quantum Computers </p>
+          <p>Vue.js/Node.js/Express.js/Socket.io/PostgreSQ</p>
         </div>
       </div>
       <div className="experience-overview">
         <img src={dell} className='logo-dell'></img>
         <div className='experience-overview-text'> 
           <Link to="/dell" > <Tab value="Dell" label="Cloud Computing @ Dell" className="experience-overview-link" /> </Link>
-          <p> description </p>
+          <p> Worked on Dell's Apex Cloud Controller to build a fullstack internal tool called a Chaos Engine that allows developers to inject failures into the Dell's On-Premise Cloud Control system to see how it responds. </p>
+          <p>MongoDB/Express.js/Angular.js/Node.js</p>
         </div>
       </div>
     </div>
--- src/pages/Experiences/Google.jsx
+++ src/pages/Experiences/Google.jsx
@@ -19,11 +19,14 @@ const Google = () => (
         </div>
         <div className="experience-text-wrapper">
           <p className='subheading'>Summary</p>
-          <p className='description-text'>insert description here</p>
+          <p className='description-text'>During my internship at Google Search, I added more useful information to car related searches! For all car searches, you can now see the MSRP data as well as other important information such as MPGe and Horsepower.  </p>
+          <p className='description-text'>For electric cars, you can now see Fastest Charging information as well as Financial Incentives for electric cars in the US.</p>
+          <p className='description-text'>Throughout this project, I worked cross-functionally with a PM, UX Designer as well as two other engineers. </p>
+          <p className='description-text'>Now that the project is launched, you can view it online if you search an electric car such as the <a href="https://www.google.com/search?sca_esv=292e527dce65be8d&sxsrf=ACQVn08w1AYGnXnkqZYjInXMGdKqj3hsrg:1712685858037&q=2023+Rivian+R1S&stick=H4sIAAAAAAAAAONgVeLVT9c3NCzOyK4sNi-qMuIpyizLTMxTKCkqTc4-xQiRzUnOiS80rUqB8bMtk0uy8rKNkeVNDM1NYXyYaY8Y1zFxC7z8cU9YagnTpDUnrzHOZeIS8MnPL07NqQxKzUksSU0JyRcS42JzzSvJLKkU4pHi4uLQz9U3SC4rNBaazMjFHZxaEpLvm5-SmVYp1Mwo1MCIRb8EXD-fFA8XF0h_epZFWUquUBCqAc5Cjlycvqm5SalFxf5pQiZcXM75OTmpySWZ-XlCalIqXEr6yXAB_cTSkvzc_KTMnNT43PyU1Jz4ytTEomKlSCO3XZemnWNzEGQAgiXZwQ5SGlqCXGwu-bmJmXmCN2cxHJD_895eS5iLIySxIj8vP7dSMNMwVXHZ_nR7JU5OoB6Hgyve2WsxTGBibNq34hAbBwejAIMREwdDFQPPIlZ-IwMjY4UgSCwEGQZPYGMEABCBVPKmAQAA&sa=X&lei=IoMVZoDJAd2m5NoPmeS28AI">Rivian R1T</a> and the Tesla Model 3. Over the summer, the project was expected to reach an estimated 100k new monthly average users!</p>
           <p className='subheading'>Roadblocks</p>
-          <p className='description-text'>insert description here</p>
+          <p className='description-text'>This project required migrating the current desktop version of a UI component that renders when cars are searched from a legacy framework to a new custom framework. This framework would affect several teams' work so every decision I made had to take into consideration the possible consequences to other teams. This required a lot of time-consuming communication with teams across Google, slowing down the overall workflow over the summer. </p>
           <p className='subheading'>Skills</p>
-          <p className='description-text'>insert description here</p>
+          <p className='description-text'>C++, Java, Javascript</p>
         </div>
         
       </div>
