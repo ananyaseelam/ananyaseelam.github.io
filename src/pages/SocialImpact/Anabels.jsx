6136b60888a6c68eb53cbf026faaa6a5df092678
as2759 <as2759@cornell.edu>
2024-04-14 14:53:02 -0400

    Added hack stuff"

--- src/pages/SocialImpact/Anabels.jsx
+++ src/pages/SocialImpact/Anabels.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import NavBar from '../../components/NavBar/NavBar';
+import store from '../assets/anabels_store.png';
 import '../Page.css';
 
 const Anabels = () => (
@@ -8,14 +9,12 @@ const Anabels = () => (
     <div className="content-container">
     <p className="header"> Anabels Grocery </p>
       <div className="full-experience-wrapper">
-        <div className='experience-image'></div>
+        <img className='anabels-image' src= {store} ></img>
         <div className="experience-text-wrapper">
           <p className='subheading'>Summary</p>
-          <p className='description-text'>insert description here</p>
-          <p className='subheading'>Roadblocks</p>
-          <p className='description-text'>insert description here</p>
-          <p className='subheading'>Skills</p>
-          <p className='description-text'>insert description here</p>
+          <p className='description-text'>During Fall of 2022, I took a practicum course at Cornell where I learnt about food justice and helped ran a justice based student-led grocery store on campus. </p>
+          <p className='description-text'>I've been volunteering in the store ever since and have spent several hours stocking, checking people out and cleaning the store! </p>
+          <p className='description-text'>Through my work at Anabel's I've learnt how to advocate against food insecurity and how our current systems are built to further food injustice.</p>
         </div>
         
       </div>
--- src/pages/SocialImpact/Hack.jsx
+++ src/pages/SocialImpact/Hack.jsx
@@ -1,22 +1,40 @@
 import React from 'react';
 import NavBar from '../../components/NavBar/NavBar';
 import '../Page.css';
+import award from '../assets/hackpic.png';
+import boom from '../assets/hackboompic.png';
+import showcase from '../assets/showcase.png';
+import { Link, useHistory, useLocation } from 'react-router-dom';
+import Tab from '../../../node_modules/@mui/material/Tab';
 
 const Hack = () => (
   <div className="container">
     <NavBar />
     <div className="content-container">
     <p className="header"> Hack4Impact </p>
-      <div className="full-experience-wrapper">
-        <div className='experience-image'></div>
-        <div className="experience-text-wrapper">
-          <p className='subheading'>Summary</p>
-          <p className='description-text'>insert description here</p>
-          <p className='subheading'>Roadblocks</p>
-          <p className='description-text'>insert description here</p>
-          <p className='subheading'>Skills</p>
-          <p className='description-text'>insert description here</p>
+    <div className="two-column-layout">
+      <div className="experience-overview">
+        <img src={award} className='experience-image'></img>
+        <div className='experience-overview-text'> 
+          <Link to="/socialimpact" > <Tab value="codirector" label="Co-Director of Hack4Impact" className="experience-overview-link" /> </Link>
+          <p> Led a team of over 60 student developers, designers, and business members and ran the Cornell Chapter of Hack4Impact for one year!</p>
         </div>
+      </div>
+      <div className="experience-overview">
+        <img src={boom} className='experience-image'></img>
+        <div className='experience-overview-text'> 
+          <Link to="/socialimpact" > <Tab value="techlead" label="Technical Lead for Earth Law Center" className="experience-overview-link" /> </Link>
+          <p> Built a fullstack website for the Earth Law Center to easily fill out eco-centric law and letter templates. </p>
+        </div>
+      </div>
+      <div className="experience-overview">
+        <img src={showcase} className='experience-image'></img>
+        <div className='experience-overview-text'> 
+          <Link to="/socialimpact" > <Tab value="dev" label="Developer" className="experience-overview-link" /> </Link>
+          <p> Helped build software products for Farmworkers Justice and Life Camp! </p>
+          {/* <p>MongoDB/Express.js/Angular.js/Node.js</p> */}
+        </div>
+      </div>
         
       </div>
     </div>
@@ -24,6 +42,5 @@ const Hack = () => (
   
 );
 
-
-
 export default Hack;
+
